423bc7b350908d7ef4e7cdeb528843ca33c2f440
Chai.Narukulla <chaitanyanarukulla@gmail.com>
2019-06-18 17:50:13 -0700

    Profile page Updated

--- src/main/java/com/chai/codefellowship/codefellowship/AppUserController.java
+++ src/main/java/com/chai/codefellowship/codefellowship/AppUserController.java
@@ -8,12 +8,12 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.view.RedirectView;
 
 import java.security.Principal;
 import java.util.ArrayList;
+import java.util.Date;
 
 @Controller
 public class AppUserController {
@@ -24,26 +24,55 @@ public class AppUserController {
     @Autowired
     PasswordEncoder bCryptPasswordEncoder;
 
+    // bulding up user info and saving into db
 
-    @PostMapping("/singUp")
-    public RedirectView createUser(String username, String password,String fname, String lname, String dob, String bio) {
+    @PostMapping("/signup")
+    public RedirectView createUser(String username, String password, String fname, String lname, Date dob, String bio) {
         AppUser newUser = new AppUser(username, bCryptPasswordEncoder.encode(password),fname,lname,dob,bio);
         appUserRepository.save(newUser);
         Authentication authentication = new UsernamePasswordAuthenticationToken(newUser, null, new ArrayList<>());
         SecurityContextHolder.getContext().setAuthentication(authentication);
-        return new RedirectView("/myProfile");
+        return new RedirectView("/");
     }
-
+//  // Home page
+//    @GetMapping("/")
+//    public RedirectView goLogin(){
+//        return new RedirectView("/login");
+//    }
+  //Login page
     @GetMapping("/login")
-    public String getLoginPage() {
-        return "login.html";
+    public String getLoginPage() { return "login"; }
+
+  //users page
+    @GetMapping("/users")
+    public String getAllUsers(Model m) {
+        Iterable<AppUser> user = appUserRepository.findAll();
+        m.addAttribute("user", user);
+        return "users.html";
     }
 
+//    @PostMapping("/login")
+//    public RedirectView
 
-    @GetMapping("/singUp")
-    public String getSignUpForm(){
-        return "singUp.html";
+  //sing up page
+    @GetMapping("/signup")
+    public String getAddSignUpForm() {
+        return "signup";
+    }
 
+  //Profile page
+    @GetMapping("/profile")
+    public String getMyProfileInfo(Principal p, Model m) {
+        AppUser user = appUserRepository.findByUsername(p.getName());
+        m.addAttribute("user", user);
+        return "profile";
+    }
+
+    @RequestMapping(value="details/{id}",method = RequestMethod.GET)
+    public String getOneUserDetail(@PathVariable Long id, Model m){
+        AppUser user = appUserRepository.findById(id).get();
+        m.addAttribute("user", user);
+        return "details";
     }
 
 }
